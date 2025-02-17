74227287fb3785ff1ecc477b493808e920ebcc92
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-17 11:02:44 +0530

    Initial commit

---
+++ src/main/java/com/e_waste/e_waste/EWasteApplication.java
@@ -0,0 +1,13 @@
+package com.e_waste.e_waste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWasteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_waste/e_waste/EWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_waste.e_waste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
