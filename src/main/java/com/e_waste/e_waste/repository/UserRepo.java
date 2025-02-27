4e4ac9d5b10b66fcb93e24e4f59f96e44f95d0f2
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:30:15 +0530

    user repo

--- src/main/java/com/e_waste/e_waste/repository/UserRepo.java
+++ src/main/java/com/e_waste/e_waste/repository/UserRepo.java
@@ -1,4 +1,10 @@
 package com.e_waste.e_waste.repository;
 
-public class UserRepo {
+import com.e_waste.e_waste.entity.UserSignUpEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserSignUpEntity, Integer> {
+    public UserSignUpEntity findByName(String name);
 }
