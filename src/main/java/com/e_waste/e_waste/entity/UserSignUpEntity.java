bfcfabd8de562112237b9f7ab98486d49f453d85
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:28:23 +0530

    sign up data

--- src/main/java/com/e_waste/e_waste/entity/UserSignUpEntity.java
+++ src/main/java/com/e_waste/e_waste/entity/UserSignUpEntity.java
@@ -1,4 +1,74 @@
 package com.e_waste.e_waste.entity;
 
-public class UserAccountEntity {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class UserSignUpEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    private String name;
+
+
+    private String email;
+
+
+    private String password;
+
+   @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "real_id", cascade = CascadeType.ALL)
+    private List<SellerPD> seller_personal_details;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
