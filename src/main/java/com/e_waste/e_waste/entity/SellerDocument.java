2210d38e2e69b2a09f062ec9b54959283227dbf4
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:27:19 +0530

    document details of seller

--- src/main/java/com/e_waste/e_waste/entity/SellerDocument.java
+++ src/main/java/com/e_waste/e_waste/entity/SellerDocument.java
@@ -1,4 +1,18 @@
 package com.e_waste.e_waste.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
 public class SellerDocument {
+    private int document_id;
+    @ManyToOne
+    private SellerPD seller_id;
+    private String  document_url;
+    private DocStatus status;
+    @CreationTimestamp  // Automatically sets the current timestamp
+    @Column(name = "uploaded_at", updatable = false)
+    private LocalDateTime uploadedAt;
 }
