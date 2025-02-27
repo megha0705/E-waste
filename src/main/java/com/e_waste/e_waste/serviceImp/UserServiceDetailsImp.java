e1ab2478158a41e9dee4044afcafe524dfb85835
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:33:15 +0530

    building user

--- src/main/java/com/e_waste/e_waste/serviceImp/UserServiceDetailsImp.java
+++ src/main/java/com/e_waste/e_waste/serviceImp/UserServiceDetailsImp.java
@@ -1,4 +1,25 @@
 package com.e_waste.e_waste.serviceImp;
 
-public class UserServiceDetailsImp {
+import com.e_waste.e_waste.entity.UserSignUpEntity;
+import com.e_waste.e_waste.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceDetailsImp implements UserDetailsService {
+@Autowired
+    UserRepo repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserSignUpEntity user = repo.findByName(username);
+        if(user != null){
+            return User.builder().username(user.getName()).password(user.getPassword()).roles(user.getRole().name()).build();
+        }
+
+        throw new UsernameNotFoundException("user is not found" + username);
+    }
 }
