86875557bff29fb514c46517a01ff42da8830ca4
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:32:29 +0530

    service function imp

--- src/main/java/com/e_waste/e_waste/serviceImp/UserImp.java
+++ src/main/java/com/e_waste/e_waste/serviceImp/UserImp.java
@@ -1,4 +1,29 @@
 package com.e_waste.e_waste.serviceImp;
 
-public class UserImp {
+import com.e_waste.e_waste.entity.Role;
+import com.e_waste.e_waste.entity.UserSignUpEntity;
+import com.e_waste.e_waste.repository.UserRepo;
+import com.e_waste.e_waste.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserImp implements UserService {
+    @Autowired
+    UserRepo repo;
+
+    public static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Override
+    public void createUser(String name, String email, String password, Role role) {
+        UserSignUpEntity user = new UserSignUpEntity();
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        user.setName(name);
+        user.setRole(role);
+        repo.save(user);
+
+
+    }
 }
