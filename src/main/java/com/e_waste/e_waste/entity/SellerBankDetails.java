3c829f5e630f7b6d9257197e91e22e1ffb4cd336
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-02-27 20:25:51 +0530

    bank details of the seller

--- src/main/java/com/e_waste/e_waste/entity/SellerBankDetails.java
+++ src/main/java/com/e_waste/e_waste/entity/SellerBankDetails.java
@@ -1,4 +1,14 @@
 package com.e_waste.e_waste.entity;
 
+import javax.persistence.ManyToOne;
+
 public class SellerBankDetails {
+    private int id;
+    @ManyToOne
+    private SellerPD seller_id;
+    private String account_holder_name;
+    private String bank_name;
+    private String account_number;
+    private String ifsc_code;
+
 }
