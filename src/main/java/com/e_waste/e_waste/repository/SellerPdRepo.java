58fc8e993bbb0a9fbb306f9dc7b65c5c2e7090cd
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-03-08 21:26:58 +0530

    repository for seller personal details

--- src/main/java/com/e_waste/e_waste/repository/SellerPdRepo.java
+++ src/main/java/com/e_waste/e_waste/repository/SellerPdRepo.java
@@ -1,4 +1,9 @@
 package com.e_waste.e_waste.repository;
 
-public interface SellerPdRepo {
+import com.e_waste.e_waste.entity.SellerPD;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerPdRepo  extends JpaRepository<SellerPD , Integer> {
 }
