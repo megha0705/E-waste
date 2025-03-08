b803e05f0f40a5f809214d881f19b7df3aaa2bea
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-03-08 21:26:30 +0530

    repository for seller docs table:

--- src/main/java/com/e_waste/e_waste/repository/SellerDocsRepo.java
+++ src/main/java/com/e_waste/e_waste/repository/SellerDocsRepo.java
@@ -1,4 +1,9 @@
 package com.e_waste.e_waste.repository;
 
-public interface SellerDocsRepo {
+import com.e_waste.e_waste.entity.SellerDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerDocsRepo extends JpaRepository<SellerDocument , Integer> {
 }
