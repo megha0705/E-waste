ad1e6472a31edd7fea40aa0d7934b2c6b13f9782
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-03-08 21:25:33 +0530

    created function for saving user pan and gst number and doc url

--- src/main/java/com/e_waste/e_waste/service/UserService.java
+++ src/main/java/com/e_waste/e_waste/service/UserService.java
@@ -1,7 +1,11 @@
 package com.e_waste.e_waste.service;
 
 import com.e_waste.e_waste.entity.Role;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     public void createUser(String name , String email, String password, Role role);
+    public void  userIdProof(String gstNumber, String pan_number , int userRealId);
+    public void userIdProofDocs(String IDProofPathUrl);
 }
